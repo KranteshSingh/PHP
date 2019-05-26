7f8f25310c98ccc3165897e11c70b1b83aa9ab3f
Krantesh Singh <34528823+KranteshSingh@users.noreply.github.com>
2019-05-26 07:02:04 +0530

    first commit

---
+++ LifestyleStore/includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <center>
+            <p>Copyright &copy; Lifestyle Store. All Rights Reserved   | Krantesh Singh |   Contact Us: +91 7275139737</p>	
+        </center>
+    </div>
+</footer>
