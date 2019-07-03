01f4945b83a1597b59bdc06dcc9b3e9526ee27be
Krantesh Singh <krantesh@gmail.com>
2019-07-03 15:16:32 +0530

    LearnPHP

---
+++ LearnPHP/_practical-app/1.php
@@ -0,0 +1,39 @@
+<?php include "functions.php" ?>
+<?php include "includes/header.php" ?>
+
+	<section class="content">
+
+	<aside class="col-xs-4">
+
+	<?php Navigation();?>
+			
+			
+	</aside><!--SIDEBAR-->
+
+
+<article class="main-content col-xs-8">
+		
+
+
+		<?php  
+
+
+		/* 
+		   Step 1:  Use the Echo Function to say hello with html h1                  tags embedded inside php.
+
+
+		   Step 2: Write a comment above the echo function and explain
+		   what that function is doing.
+
+
+		   */
+			// This is a single line comment and below is Heading 1 Text Saying Hello to YOU
+		   echo "<h1>Hello, This is Krantesh Singh</h1>"
+
+		?>
+
+	
+
+		</article><!--MAIN CONTENT-->
+
+<?php include "includes/footer.php" ?>
